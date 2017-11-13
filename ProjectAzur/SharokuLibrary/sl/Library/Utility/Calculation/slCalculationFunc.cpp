6b99e7a729fc006ea614ec28a3dc096165fe2798
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-11-13 19:53:13 +0900

    [enh #7] ２Dにおける行列計算関数の実装
    -計算関数用ファイル追加
    -2D行列計算関数の実装

---
+++ ProjectAzur/SharokuLibrary/sl/Library/Utility/Calculation/slCalculationFunc.cpp
@@ -0,0 +1,38 @@
+﻿//==================================================================================================================================//
+//!< @file		slCalculationFunc.cpp
+//!< @brief		計算関連の関数をまとめたファイルの実装
+//!< @author	T.Haga
+//!< @data		作成日時：2017/11/13	更新履歴：
+//==================================================================================================================================//
+
+#include "slCalculationFunc.h"
+
+namespace sl
+{
+namespace calculation
+{ 
+
+void Calculate2DMatWorld(D3DXMATRIX* pMatWorld
+						, const D3DXVECTOR2& rPos
+						, const D3DXVECTOR3& rScale
+						, float angle)
+{
+	D3DXMATRIX matTranslate,matRotate;
+	D3DXMatrixIdentity(pMatWorld);
+	D3DXMatrixScaling(pMatWorld, rScale.x, rScale.y, 1.0f);
+	D3DXMatrixRotationZ(&matRotate, angle);
+	D3DXMatrixMultiply(pMatWorld, pMatWorld, &matRotate);
+	D3DXMatrixTranslation(&matTranslate, rPos.x, rPos.y, 0);
+	D3DXMatrixMultiply(pMatWorld, pMatWorld, &matTranslate);
+
+}
+
+
+
+
+}	// namespace calculation
+}	// namespace sl
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
---
+++ ProjectAzur/SharokuLibrary/sl/Library/Utility/Calculation/slCalculationFunc.h
@@ -0,0 +1,41 @@
+﻿//==================================================================================================================================//
+//!< @file		slCalculationFunc.h
+//!< @brief		計算関連の関数をまとめたファイルのヘッダ
+//!< @author	T.Haga
+//!< @data		作成日時：2017/11/13	更新履歴：
+//==================================================================================================================================//
+
+#ifndef SL_CALCULATION_HFUNC_H
+#define SL_CALCULATION_HFUNC_H
+
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+namespace sl
+{
+namespace calculation
+{
+
+
+/**
+* 2Dにおけるワールド座標を計算する関数
+* @param[out]	pMatWorld	計算したワールド座標を格納する変数
+* @param[in]	rPos		位置座標	
+* @param[in]	rScale		スケール値.デフォルトが1.0倍
+* @param[in]	angle		角度.デフォルト値は0.0ｆ
+*/
+void Calculate2DMatWorld(D3DXMATRIX* pMatWorld
+						, const D3DXVECTOR2& rPos
+						, const D3DXVECTOR3& rScale = D3DXVECTOR3(1.0f, 1.0f, 1.0f)
+						, float angle = 0.0f);
+
+
+}	// namespace calculation
+}	// namespace sl
+
+#endif	// SL_CALCULATION_HFUNC_H
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
+
