df2556873ba4e591463bc8bd36a3b64afafddcac
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-11-21 03:03:04 +0900

    [enh #18]ゲーム内イベント処理関連の追加と実装
    -イベント処理関連の追加と実装-

---
+++ ProjectAzur/ProjectAzur/MainGame/EventManager/paEventListener.h
@@ -0,0 +1,83 @@
+﻿//==================================================================================================================================//
+//!< @file		paEventListener.h
+//!< @brief		pa::EventListenerクラスのヘッダ
+//!< @author	T.Haga
+//!< @data		作成日時：2017/11/21	更新履歴：2017/11/22
+//==================================================================================================================================//
+
+#ifndef PA_EVENT_LISTENER_H
+#define PA_EVENT_LISTENER_H
+
+#include <map>
+#include <deque>
+#include <string>
+#include <functional>
+
+namespace pa
+{
+
+//===================================================================================//
+//!< イベントマネージャーからイベントを受け取り、
+//!< そのイベントを別クラスに通知するクラス
+//!< また同期イベントが通知された際は
+//!< 登録されたイベント関数で処理を行う
+//===================================================================================//
+class EventListener final
+{
+
+public:
+	/** Constructor */
+	EventListener() = default;
+
+	/** Destructor */
+	~EventListener() = default;
+
+	/**
+	* 同期イベントの処理に使用する関数を登録する関数
+	* @param[in] rEventName					イベント名
+	* @param[in] rCallBackFunc				イベントの際によびだすコールバック関数
+	*/
+	void RegisterSynEventFunc(const std::string& rEventName, const std::function<void(void)>& rCallBackFunc);
+	
+	/** 
+	* 登録している同期イベントの処理関数を呼び出す関数
+	* @param[in] rEventName		処理を行うイベント名
+	*/
+	void CallSynEventFunc(const std::string& rEventName);  
+
+	/** 現在受けとっているイベントを全て削除する関数 */
+	void DeleteCurrentEvent(void); 
+
+	/**
+	* 受け取っているイベントが現在空なのか、もしくはあるのか確認する関数
+	* @return 結果 true→空である false→空でない
+	*/
+	inline bool EmptyCurrentEvent(void) { return m_CurrentEvent.empty(); }
+
+	/**
+	* Getter
+	* @return 現在イベントマネージャーから受け取っているイベントを格納したdeque
+	*/
+	inline const std::deque<std::string>& GetCurrentEvent(void) { return m_CurrentEvent; };
+
+private:
+	friend class EventManager;
+
+	std::deque<std::string>									m_CurrentEvent;					//!< 現在ゲーム中で発生したイベントを格納しておくdeque
+	std::map<std::string, std::function<void(void)>>		m_SynEventFunc;					//!< 同期イベント処理を格納しておくmap
+
+	/** 
+	* 現在発生しているイベントを受け取る関数。イベントマネージャーが呼び出す関数
+	* @param[in] rEventName		現在発生しているイベント名
+	*/
+	void ReceiveEvent(const std::string& rEventName); 
+
+};	// class EventListener
+
+}	// namespace pa
+
+#endif	// PA_EVENT_LISTENER_H
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
---
+++ ProjectAzur/ProjectAzur/MainGame/EventManager/paEventManager.h
@@ -0,0 +1,97 @@
+﻿//==================================================================================================================================//
+//!< @file		paEventManager.h
+//!< @brief		pa::EventManagerクラスのヘッダ
+//!< @author	T.Haga
+//!< @data		作成日時：2017/11/21	更新履歴：2017/11/22
+//==================================================================================================================================//
+
+#ifndef PA_EVENT_MANAGER_H
+#define PA_EVENT_MANAGER_H
+
+#include <string>
+#include <map>
+#include <list>
+#include <deque>
+#include "sl/Library/Singleton/slBasicSingleton.h"
+#include "sl/Library/Utility/slDefine.h"
+
+namespace pa
+{
+
+class EventListener;
+
+//===================================================================================//
+//!< イベントを管理するクラス
+//===================================================================================//
+class EventManager final : public sl::BasicSingleton<EventManager>
+{
+
+public:
+	/**
+	* 初期化関数
+	* 登録しているイベント、及びそれに対応するリスナーのポインタを全て削除する
+	*/
+	void Initialize();
+
+	/**
+	* イベント処理更新関数
+	* 毎フレームをよんで、イベントをチェック、処理する関数
+	*/
+	void Update();
+
+	/*
+	* イベントとそれに対応するリスナーを登録する関数
+	* なおイベントが登録されていない場合は自動的に追加する
+	* @param[in] rEventName		登録したいイベントor リスナーが登録したいイベントタイプ
+	* @param[in] pListener		上記のイベントタイプに登録したいEventListenerクラスのインスタンスへのポインタ
+	*/
+	void RegisterEventListener(const std::string& rEventName, EventListener* pListener); 
+
+	/*
+	* 現在登録しているイベントを削除する関数
+	* @param[in] rEventName 削除したいイベント名
+	*/
+	void DeleteEvent(const std::string& rEventName);
+
+	/**
+	* 現在登録しているイベント群から指定したリスナーを削除する関数
+	* @param[in] rEventName		登録したいイベントor リスナーが登録したいイベントタイプ
+	* @param[in] pListener		削除したいリスナー
+	*/
+	void DeleteEventListener(const std::string& rEventName, EventListener* pListener);
+	
+	/**
+	* 同期的なイベントをLisnerに発信する関数
+	* @param[in] rEventType 発信したいイベント名
+	*/
+	void TriggerSynEvent(const std::string& rEventName);  
+
+	/**
+	* ゲーム中で発生したイベントを受け取る関数
+	* @param[in] rEventType 発生したイベント名
+	*/
+	void ReceiveEvent(const std::string& rEventName);  
+   
+private:
+	friend class sl::StaticCreation<EventManager>;
+
+	std::map<std::string, std::list<EventListener*>>	m_pEventListener;		//!< 登録している通常イベントとそれに対応するリスナーのポインタmap
+	std::deque<std::string>								m_CurrentEvent;			//!< 現在ゲーム中で発生したイベントを格納しておくdeque
+
+	/** Constructor */
+	EventManager() = default;
+
+	/** Destructor */
+	~EventManager() = default;
+
+	SL_DISALLOW_COPY_AND_ASSIGN(EventManager);
+
+};	// class EventManager
+
+}	// namespace pa
+
+#endif	//PA_EVENT_MANAGER_H
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
