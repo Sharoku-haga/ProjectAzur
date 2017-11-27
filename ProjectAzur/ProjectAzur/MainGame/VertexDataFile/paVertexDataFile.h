37f15b988ea4304448e9adace48154d690a58f53
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-11-28 01:30:01 +0900

    [enh #24]関数の追加

--- ProjectAzur/ProjectAzur/MainGame/VertexDataFile/paVertexDataFile.h
+++ ProjectAzur/ProjectAzur/MainGame/VertexDataFile/paVertexDataFile.h
@@ -8,11 +8,19 @@
 #ifndef PA_VERTEX_DATA_FILE_H
 #define PA_VERTEX_DATA_FILE_H
 
+#include <string>
+#include <map>
+
 namespace pa
 {
 
+struct VertexData
+{
+
+};	// struct VertexData
+
 //===================================================================================//
-//!< ツールが作成するVertexDataファイルを管理するクラス 
+//!< ツールが作成するVertexに関するデータファイルを管理するクラス 
 //===================================================================================//
 class VertexDataFile
 {
@@ -23,6 +31,22 @@ public:
 
 	/** Destructor */
 	~VertexDataFile();
+
+	/**
+	* データファイルをロードする関数
+	* @param[in] rFilePath データファイルのパス
+	* @return 結果 true→成功 false→失敗
+	*/
+	bool LoadDataFile(const std::string& rFilePath);
+
+	VertexData& GetVertexData(const std::string& rFilePath, int id);
+
+	void ReleaseDataFile(const std::string& rFilePath);
+
+	void ReleaseDataFileALL();
+
+private:
+	std::map< std::string, std::map<int, VertexData> > m_VertexDatas;			//!< VertexDataを格納しているmap.intはID
 
 };	// class VertexDataFile
 
