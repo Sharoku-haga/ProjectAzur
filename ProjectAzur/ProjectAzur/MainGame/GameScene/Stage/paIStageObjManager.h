bf731ed103759620aab664285f076e393b2a1a1e
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-12-09 15:02:38 +0900

    [enh #49] 作業必要ファイル追加

---
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/paIStageObjManager.h
@@ -0,0 +1,29 @@
+﻿//==================================================================================================================================//
+//!< @file		paIStageObjManager.h		
+//!< @brief		pa;;IStageObjManagerインターフェースのヘッダ
+//!< @author	T.Haga
+//!< @data		作成日時：2017/12/09	更新履歴：
+//==================================================================================================================================//
+
+#ifndef PA_ISTAGE_OBJ_MANAGER_H
+#define PA_ISTAGE_OBJ_MANAGER_H
+
+#include "sl/Library/Utility/slDefine.h"
+
+interface IStageObjManager
+{
+
+public:
+	/** Constructor */
+	IStageObjManager() = default;
+
+	/** Destructor */
+	virtual ~IStageObjManager() = default;
+
+};	// interface IStageObjManager
+
+#endif	// PA_ISTAGE_OBJ_MANAGER_H
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
