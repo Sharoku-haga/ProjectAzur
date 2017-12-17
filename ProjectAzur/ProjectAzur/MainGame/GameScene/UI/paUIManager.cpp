bc74b972a940ffe3051d4d12a83d9b39e4051e6a
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-12-17 23:06:27 +0900

    [enh #69] UIManagerクラスとIUIクラスの実装
    -UIのインタフェースクラスの実装
    -UIを管理するクラスの実装

---
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/UI/paUIManager.cpp
@@ -0,0 +1,56 @@
+﻿//==================================================================================================================================//
+//!< @file		paUIManager.cpp
+//!< @brief		pa::UIManagerクラス実装
+//!< @author	T.Haga
+//!< @data		作成日時：2017/12/17	更新履歴：2017/12/18
+//==================================================================================================================================//
+
+/* Includes --------------------------------------------------------------------------------------------------- */
+
+#include "paUIManager.h"
+#include "paIUI.h"
+
+namespace pa
+{
+
+/* Public Functions ------------------------------------------------------------------------------------------- */
+
+UIManager::UIManager(const sl::DrawingID& rIDs
+					, const std::string& rResrcDataFilePath)
+	: m_DrawingID(rIDs)
+	, m_ResrcDataFilePath(rResrcDataFilePath)
+{}
+
+UIManager::~UIManager()
+{
+	Finalize();
+}
+
+void UIManager::Initialize()
+{
+	for(auto& pUI : m_pUI)
+	{
+		pUI->Initialize();;
+	}
+}
+
+void UIManager::Update()
+{
+
+}
+
+void UIManager::Finalize()
+{
+	for(auto& pUI : m_pUI)
+	{
+		pUI->Finalize();
+	}
+
+	std::vector<sl::UniquePtr<IUI, sl::DefaultDeleter<IUI>>>().swap(m_pUI);
+}
+
+}	// namespace pa
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
