b6141cbbf97a45adcdf74b8c3f99477779599972
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-12-28 21:10:29 +0900

    [enh #76]FishShapeCreatorクラスの実装

---
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/Fish/FishCreator/paFishShapeCreator.cpp
@@ -0,0 +1,78 @@
+﻿//==================================================================================================================================//
+//!< @file		paFishShapeCreator.cpp
+//!< @brief		pa::FishShapeCreatorクラスの実装
+//!< @author	T.Haga
+//!< @data		作成日時：2017/12/28	更新履歴：2017/12/29
+//==================================================================================================================================//
+
+/* Includes --------------------------------------------------------------------------------------------------- */
+
+#include "paFishShapeCreator.h"
+#include "../../../../DrawingResrcDataFile/paDrawingResrcDataFile.h"
+
+namespace pa
+{
+
+/* Unnamed Namespace ------------------------------------------------------------------------------------------ */
+
+namespace
+{
+
+const int		m_DrawingDataID = 3;		// 描画データID
+
+}
+
+/* Public Functions ------------------------------------------------------------------------------------------- */
+
+FishShapeCreator::FishShapeCreator(const sl::DrawingID&	rIDs
+									, const char*	pResrcDataFilePath)
+	: m_rLibrary(sl::DX11GraphicsLibrary::Instance())
+	, m_BaseShapeID(rIDs)
+	, m_pResrcDataFilePath(pResrcDataFilePath)
+{}
+
+FishShapeCreator::~FishShapeCreator()
+{}
+
+sl::UniquePtr<FishShape> FishShapeCreator::CreateFishShape()
+{
+	// モデルの生成
+	std::vector<sl::ModelID> modelIDs;
+	CreateModel(modelIDs);
+
+	// UVアニメの生成
+	std::vector<sl::UVAnimeID> uvAnimeIDs;
+	CreateUVAnime(uvAnimeIDs);
+
+	return sl::UniquePtr<FishShape>(new FishShape(m_BaseShapeID, modelIDs, uvAnimeIDs));
+}
+
+/* Private Functions ------------------------------------------------------------------------------------------ */
+
+void FishShapeCreator::CreateModel(std::vector<sl::ModelID>& rModelIDs)
+{
+	/** 現在はモデル1つのみ生成 */
+	DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(m_pResrcDataFilePath, m_DrawingDataID);
+	sl::Model2DCreationData data(rResrc.m_Width, rResrc.m_Height, rResrc.m_UVRect, true);
+	rModelIDs.emplace_back(m_rLibrary.CreateDXModel2D(data));
+}
+
+void FishShapeCreator::CreateUVAnime(std::vector<sl::UVAnimeID>& rUVAnimeIDs)
+{
+	DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(m_pResrcDataFilePath, m_DrawingDataID);
+
+	/** @todo 現在はアニメを1つだけ生成 */
+	sl::UVAnimeCreationData animeData;
+	animeData.m_AnimePatternCount = 1;
+	animeData.m_AnimeWaitingTime = 0;
+	animeData.m_StartUV = rResrc.m_UVRect;
+	animeData.m_ScrollTu = 0.0f;
+	animeData.m_ScrollTv = 0.0f;
+	rUVAnimeIDs.emplace_back(m_rLibrary.CreateAnimatoin(animeData));
+}
+
+}	// namespace pa
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
---
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/Fish/FishCreator/paFishShapeCreator.h
@@ -0,0 +1,70 @@
+﻿//==================================================================================================================================//
+//!< @file		paFishShapeCreator.h
+//!< @brief		pa::FishShapeCreatorクラスのヘッダ
+//!< @author	T.Haga
+//!< @data		作成日時：2017/12/28	更新履歴：2017/12/29
+//==================================================================================================================================//
+
+#ifndef PA_FISH_SHAPE_CREATOR_H
+#define PA_FISH_SHAPE_CREATOR_H
+
+#include <vector>
+#include "sl/Library/Graphics/DX11GraphicsLibrary/slDX11GraphicsLibrary.h"
+#include "sl/Library/SmartPointer/UniquePtr/slUniquePtr.h"
+#include "../FishShape/paFishShape.h"
+
+namespace pa
+{
+
+//===================================================================================//
+//!< FishShapeクラスを作成するクラス
+//===================================================================================//
+class FishShapeCreator
+{
+
+public:
+	/** 
+	* Constructor
+	* @param[in] rIDs					描画のID群
+	* @param[in] pResrcDataFilePath		リソースデータのファイルパス
+	*/
+	FishShapeCreator(const sl::DrawingID&	rIDs
+					, const char*	pResrcDataFilePath);
+
+	/** Destructor */
+	~FishShapeCreator();
+
+	/** 
+	* FishShapeクラスを生成する関数 
+	* @todo 現在は引数はないが、後に実装する予定
+	*/
+	sl::UniquePtr<FishShape> CreateFishShape();
+
+private:
+	sl::DX11GraphicsLibrary&			m_rLibrary;				//!< グラフィックスライブラリのインスタンスへの参照
+	sl::DrawingID						m_BaseShapeID;			//!< 基本形状の描画ID
+	const char*							m_pResrcDataFilePath;	//!< リソースデータファイルへのパス
+
+	/**
+	* モデルを生成する関数 
+	* @todo 後に引数に個性を追加する予定
+	* @param[out] rModelIDs 生成したモデルIDを格納する動的配列
+	*/
+	void CreateModel(std::vector<sl::ModelID>& rModelIDs);
+
+	/**
+	* UVアニメを生成する関数 
+	* @todo 後に引数に個性を追加する予定
+	* @param[out] rUVanimeIDs 生成したUVアニメIDを格納する動的配列
+	*/
+	void CreateUVAnime(std::vector<sl::UVAnimeID>& rUVAnimeIDs);
+
+};	// class FishShapeCreator
+
+}	// namespace pa
+
+#endif	// PA_FISH_SHAPE_CREATOR_H
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
