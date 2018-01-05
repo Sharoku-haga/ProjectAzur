28805d8903eb736deb3ff894991ed276f28b307d
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2018-01-06 01:32:01 +0900

    [enh #83]ColliderBaseクラスの実装

--- ProjectAzur/ProjectAzur/MainGame/GameScene/Collider/paColliderBase.cpp
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/Collider/paColliderBase.cpp
@@ -2,7 +2,7 @@
 //!< @file		paColliderBase.cpp
 //!< @brief		pa::ColliderBaseクラスの実装
 //!< @author	T.Haga
-//!< @data		作成日時：2018/01/05	更新履歴：
+//!< @data		作成日時：2018/01/05	更新履歴：2018/01/06
 //==================================================================================================================================//
 
 /* Includes --------------------------------------------------------------------------------------------------- */
@@ -14,11 +14,19 @@ namespace pa
 
 /* Public Functions ------------------------------------------------------------------------------------------- */
 
-ColliderBase::ColliderBase()
+ColliderBase::ColliderBase(const std::string& rTypeName)
+	: m_TypeName(rTypeName)
 {}
 
-ColliderBase::~ColliderBase()
-{}
+/* Protected Functions ---------------------------------------------------------------------------------------- */
+
+void ColliderBase::UpdateCurrentRectData()
+{
+	m_CurrentRectData.m_Left	= m_Pos.x + m_Size.m_Left; 
+	m_CurrentRectData.m_Top		= m_Pos.y + m_Size.m_Top;
+	m_CurrentRectData.m_Right	= m_Pos.x + m_Size.m_Right;
+	m_CurrentRectData.m_Bottom	= m_Pos.y + m_Size.m_Bottom;
+}
 
 }	// namespace pa
 
