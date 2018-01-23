a572c1849b33fe6d6ddd9e8a1f1e779a3456a0db
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2018-01-23 15:00:38 +0900

    [enh #102] ReactionMarkFactoryクラスの実装

--- ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/Fish/FishReaction/ReactionMark/paReactionMarkFactory.cpp
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/Fish/FishReaction/ReactionMark/paReactionMarkFactory.cpp
@@ -2,23 +2,62 @@
 //!< @file		paReactionMarkFactory.cpp		
 //!< @brief		pa::ReactionMarkFactoryクラスの実装
 //!< @author	T.Haga
-//!< @data		作成日時：2018/01/22	更新履歴：
+//!< @data		作成日時：2018/01/22	更新履歴：2018/01/23
 //==================================================================================================================================//
 
 /* Includes --------------------------------------------------------------------------------------------------- */
 
+#include "sl/Library/Graphics/slGraphicsDeclaration.h"
+#include "sl/Library/Graphics/DX11GraphicsLibrary/slDX11GraphicsLibrary.h"
 #include "paReactionMarkFactory.h"
+#include "../../../../../DrawingResrcDataFile/paDrawingResrcDataFile.h"
 
 namespace pa
 {
 
 /* Public Functions ------------------------------------------------------------------------------------------- */
 
-ReactionMarkFactory::ReactionMarkFactory()
-{}
+void ReactionMarkFactory::Initialize(const sl::DrawingID&	rIDs
+									, const std::string&	rResrcDataFilePath
+									, const D3DXVECTOR2&	rFishDiffPos)
+{
+	// インスタンス作成
 
-ReactionMarkFactory::~ReactionMarkFactory()
-{}
+	// HUNGER
+	{
+		int resrcDataID = 1;
+		DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(rResrcDataFilePath, resrcDataID);
+		m_pMark[HUNGER].Reset(new ReactionMark(rIDs, rResrc, HUNGER, rFishDiffPos));
+	}
+	
+	// WARNING 
+	{
+		int resrcDataID = 1;
+		DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(rResrcDataFilePath, resrcDataID);
+		m_pMark[WARNING].Reset(new ReactionMark(rIDs, rResrc, WARNING, rFishDiffPos));
+	}
+
+	// ATTACK
+	{
+		int resrcDataID = 1;
+		DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(rResrcDataFilePath, resrcDataID);
+		m_pMark[ATTACK].Reset(new ReactionMark(rIDs, rResrc, ATTACK, rFishDiffPos));
+	}
+
+	// DISCOVERY
+	{
+		int resrcDataID = 1;
+		DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(rResrcDataFilePath, resrcDataID);
+		m_pMark[DISCOVERY].Reset(new ReactionMark(rIDs, rResrc, DISCOVERY, rFishDiffPos));
+	}
+
+	// REFUGE
+	{
+		int resrcDataID = 1;
+		DrawingResrcData& rResrc = DrawingResrcDataFile::Instance().GetDrawingData(rResrcDataFilePath, resrcDataID);
+		m_pMark[REFUGE].Reset(new ReactionMark(rIDs, rResrc, REFUGE, rFishDiffPos));
+	}
+}
 
 }	// namespace pa
 
--- ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/Fish/FishReaction/ReactionMark/paReactionMarkFactory.h
+++ ProjectAzur/ProjectAzur/MainGame/GameScene/Stage/Fish/FishReaction/ReactionMark/paReactionMarkFactory.h
@@ -2,27 +2,59 @@
 //!< @file		paReactionMarkFactory.h		
 //!< @brief		pa::ReactionMarkFactoryクラスのヘッダ
 //!< @author	T.Haga
-//!< @data		作成日時：2018/01/22	更新履歴：
+//!< @data		作成日時：2018/01/22	更新履歴：2018/01/23
 //==================================================================================================================================//
 
 #ifndef PA_REACTION_MARK_FACTORY_H
 #define PA_REACTION_MARK_FACTORY_H
 
+#include <map>
+#include <sl/Library/Singleton/slUserManualSingleton.h>
+#include "sl/Library/SmartPointer/SharedPtr/slSharedPtr.h"
+#include "paReactionMark.h"
+#include "../../paFishCommonDeclaration.h"
+
 namespace pa
 {
 
 //===================================================================================//
-//!< リアクションのマークを生成するクラス
+//!< リアクションのマークを生成、管理を行うクラス
+//!< Singletonクラス
+//!< クラス生成、初期化などの処理はFishManager内にて行う
 //===================================================================================//
-class ReactionMarkFactory
+class ReactionMarkFactory : public sl::UserManualSingleton<ReactionMarkFactory>
 {
 
 public:
+	/**
+	* 初期化関数
+	* @param[in] rIDs					描画データID群
+	* @param[in] rResrcDataFilePath		リソースデータファイルへのパス
+	* @param[in] rFishDiffPos			魚との差異座標
+	*/
+	void Initialize(const sl::DrawingID&	rIDs
+					, const std::string&	rResrcDataFilePath
+					, const D3DXVECTOR2&	rFishDiffPos);
+
+	/**
+	* Getter
+	* @param[in] type リアクションマークの種類
+	* @return ReactionMarkクラスのインスタンスへのポインタ
+	*/
+	sl::SharedPtr<ReactionMark> GetMarkPointer(REACTION_MARK_TYPE type)
+	{
+		return m_pMark[type];
+	}
+
+private:
+	friend class sl::UserManualSingleton <ReactionMarkFactory>;
+	std::map<REACTION_MARK_TYPE, sl::SharedPtr<ReactionMark>>		m_pMark;			//!< ReactionMarkクラスのインスタンスへのポインタのmap
+
 	/** Constructor */
-	ReactionMarkFactory();
+	ReactionMarkFactory() = default;
 
 	/** Destructor */
-	~ReactionMarkFactory();
+	virtual ~ReactionMarkFactory() = default;
 
 };	// class ReactionMarkFactory
 
